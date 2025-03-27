bd8bdcca8c010f56387c8407f65d8509aaae8fb5
Alkar-stud <alexandre.kapps@gmail.com>
2025-03-27 14:28:18 +0100

    Add mentionslegales

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-
+    new Route("/mentionslegales", "Mentions légales", "/pages/mentionslegales.html", []),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
